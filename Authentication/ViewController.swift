efc0f3fef66625427df110e8fc764bb2201d7fc1
suhfangmbeng <suhfangmbeng@gmail.com>
2020-05-22 14:58:33 -0500

    Initial Commit

---
+++ Authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Authentication
+//
+//  Created by Suh Fangmbeng on 5/22/20.
+//  Copyright © 2020 Suh Fangmbeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
